066446eefeb849e75d9443a9e7ea1412a88d0a24
stwind <stwindfy@gmail.com>
2020-12-31 00:26:29 +0800

    chore: misc update

--- README.md
+++ README.md
@@ -16,8 +16,10 @@ Swiss Roll
 
 ![](images/wgan_gp_swiss_roll.png)
 
-References
+## References
 
 * [[1704.00028] Improved Training of Wasserstein GANs](https://arxiv.org/abs/1704.00028)
 * [[1801.04406] Which Training Methods for GANs do actually Converge?](https://arxiv.org/abs/1801.04406)
 * [[1902.03984] Improving Generalization and Stability of Generative Adversarial Networks](https://arxiv.org/abs/1902.03984)
+* [[1709.08894] On the regularization of Wasserstein GANs](https://arxiv.org/abs/1709.08894)
+* [[1609.04468] Sampling Generative Networks](https://arxiv.org/abs/1609.04468)
---
+++ ganexp/utils.py
@@ -0,0 +1,67 @@
+import numpy as np
+
+
+def slerp(p0, p1, t):
+    omega = np.arccos(np.dot(p0 / np.linalg.norm(p0), p1 / np.linalg.norm(p1)))
+    so = np.sin(omega)
+    return np.sin((1.0 - t) * omega) / so * p0 + np.sin(t * omega) / so * p1
+
+
+def make_analogy_grid(anchors):
+    n, dim = len(anchors), anchors.shape[1]
+    rows = n // 2 + 1
+    cols = n - rows + 1
+    grid = np.zeros((rows, cols, dim), dtype=np.float32)
+    for y in range(rows):
+        for x in range(cols):
+            if x == 0 or y == 0:
+                if x == 0 and y == 0:
+                    idx = 0
+                elif x == 0:
+                    idx = y * 2 - 1
+                else:
+                    idx = x * 2
+                grid[y, x, :] = anchors[idx]
+            else:
+                anal_vec = grid[y, x - 1, :] + grid[y - 1, x, :] - grid[y - 1, x - 1, :]
+                anal_unit_vec = np.nan_to_num(anal_vec / np.linalg.norm(anal_vec))
+                avg_len = (
+                    np.linalg.norm(grid[y, x - 1, :])
+                    + np.linalg.norm(grid[y - 1, x, :])
+                    + np.linalg.norm(grid[y - 1, x - 1, :])
+                ) / 3.0
+                grid[y, x, :] = avg_len * anal_unit_vec
+    return grid.reshape(-1, dim)
+
+
+def make_mine_grid(anchors, nrow, space):
+    dim = anchors.shape[1]
+    ncol = len(anchors) // nrow
+    rows = (nrow - 1) * space + 1
+    cols = (ncol - 1) * space + 1
+
+    grid = np.zeros((rows, cols, dim), dtype=np.float32)
+
+    for y in range(rows):
+        for x in range(cols):
+            if y % space == 0 and x % space == 0:
+                idx = (y // space) * ncol + (x // space)
+                grid[y, x, :] = anchors[idx]
+
+    for y in range(rows):
+        for x in range(cols):
+            if y % space == 0 and x % space != 0:
+                prev = space * (x // space)
+                cur = prev + space
+                t = (x - prev) / space
+                grid[y, x, :] = slerp(grid[y, prev, :], grid[y, cur, :], t)
+
+    for y in range(rows):
+        for x in range(cols):
+            if y % space != 0:
+                prev = space * (y // space)
+                cur = prev + space
+                t = (y - prev) / space
+                grid[y, x, :] = slerp(grid[prev, x, :], grid[cur, x, :], t)
+
+    return grid.reshape(-1, dim)
